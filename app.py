26554cb059c1aebfc9f33c127ab8b68032073447
MoKubtan <146363676+MoKubtan@users.noreply.github.com>
2023-12-07 22:06:04 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+def biggerOfTwo():
+    max_value = max(2, 4)
+    print(max_value)
+def printName():
+    print("Mohammed Kutban")  
+def null():
+    pass   
+printName()
+biggerOfTwo()    
+null()
